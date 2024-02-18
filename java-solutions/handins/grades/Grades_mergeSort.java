41c0012534af2e76f37beefadf9066e3024ac262
Frederik Lind <fred2193@stud.kea.dk>
2024-02-18 20:36:40 +0100

    grades solved - java

--- java-solutions/handins/grades/Grades_mergeSort.java
+++ java-solutions/handins/grades/Grades_mergeSort.java
@@ -1,41 +1,7 @@
-# Grades
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
-The grading scheme of the European Credit Transfer and Accumulation System (ECTS) uses 7 grades given as letters: A, B, C, D, E, FX, and F in that order, with A being the highest grade.
-
-Lektor Blomme needs your help in sorting his students by grade. Students with a higher grade should appear before students with a lower grade.
-
-In some courses, lektor Blomme was dissatisfied with the coarseness of the ECTS grading scheme. In those courses he used + and - modifiers to get more fine-grained grades as such as “A+” or “B-” with the obvious interpretation. For intance, grade C+++ is better than C++, and B+ is better than B, and B is better than B-.
-
-### Test groups
-
-There are 4 different test groups, each worth 25 points.
-
-$$
-\begin{array}{c c c}
-  \text{Group} & \text{Points} & \text{Additional constraints} \\
-  \hline
-  \text{1} & \text{25} & \text{A, B, C, D, E, F} \\
-  \text{2} & \text{25} & \text{A, B, C, D, E, FX, F}\\
-  \text{3} & \text{25} & \text{A, B, C, D, E, FX, F, followed by at most one + or -} \\
-  \text{4} & \text{25} & \text{No further restrictions} \\
-\end{array}
-$$
-
-### Input
-
-The input starts with the the number $n$ of students, an integer in the range $1 \leq n \leq 1000$. The following $n$ lines each contain the name of a student followed by a grade, separated by a single space. The name of a student is a nonempty sequence of lower-case English letters from a to z of length at most 20. A grade starts with one of the upper-case letters A, B, C, D, E, F. The letter F may be followed by an upper-case X. Then follows a (possibly empty) sequence of either `+` or `-` of length at most 10.
-
-### Output
-
-The students, one per line, ordered by grade, best students first. Students with the same grade must be ordered alphabetically.
-
-<br/>
-
-## Solution
-
-Uses a nested class that is initialized for each input line read.
-
-```java
 public class Grades_mergeSort {
     private static Map<String, Integer> Gnum = new HashMap<String, Integer>() {{
         put("A", 7);
@@ -83,10 +49,10 @@ public class Grades_mergeSort {
     }
     
     public static Grade[] sort(Grade[] arr) {
-        if (arr.length == 1) return arr;          // in case array is of length 1, it is sorted
-        aux = new Grade[arr.length];              // initialize the auxiliary array    
+        if (arr.length == 1) return arr;            // in case array is of length 1, it is sorted
+        aux = new Grade[arr.length];                // initialize the auxiliary array    
         sort(arr, 0, arr.length - 1);             // start sorting
-        return arr;                               // return the result.
+        return arr;                                 // return the result.
     }
 
     public static void main(String[] args) {
@@ -113,16 +79,11 @@ public class Grades_mergeSort {
             String[] spl = ln.split(" ");
             name = spl[0];
             String[] gSpl = spl[1].split("(?=[+-])", 2);        // split by "+/-"
-            grade = Gnum.get(gSpl[0]);                          // lookup int rep of grade chars
-            if (gSpl.length > 1) {                              // add value to modifier
+            grade = Gnum.get(gSpl[0]);                                      // lookup int rep of grade chars
+            if (gSpl.length > 1) {                                          // add value to modifier
                 int modLen = gSpl[1].length();
                 mod = gSpl[1].startsWith("+") ? modLen : -modLen;
             }
         }
     }
 }
-```
-
-```python
-
-```
